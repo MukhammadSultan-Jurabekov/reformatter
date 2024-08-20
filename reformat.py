cfe4571ef0d72c94a384282b0836bc196006a46d
MukhammadSultan-Jurabekov <jurabekovmukhammadsultan@gmail.com>
2024-08-20 16:25:42 +0100

    Word to pdf
    Code that changes File's from

--- reformat.py
+++ reformat.py
@@ -7,3 +7,17 @@ from pptx import Presentation
 import pandas as pd
 import os
 
+def word_to_pdf(docx_path, pdf_path):
+    pdfkit.from_file(docx_path, pdf_path)
+
+def txt_to_pdf(txt_path, pdf_path):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    
+    with open(txt_path, 'r') as file:
+        for line in file:
+            pdf.cell(200, 10, txt=line.encode('latin-1', 'replace').decode('latin-1'), ln=True)
+    
+    pdf.output(pdf_path)
+
